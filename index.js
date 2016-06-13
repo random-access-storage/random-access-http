f681bba3c5bea0235f129710a0be5b807e520af7
Bret Comnes <bcomnes@gmail.com>
2016-06-13 00:25:10 -0700

    Implemented an open method

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var events = require('events')
 var inherits = require('inherits')
 var http = require('http')
 var https = require('https')
+var url = require('url')
+var xtend = require('xtend')
 
 module.exports = RandomAccessMemory
 
@@ -14,6 +16,11 @@ function RandomAccessHTTP (url, opts) {
   var self = this
 
   this.url = url
+  this.urlObj = url.parse(url)
+  this.client = {
+    http: http,
+    https: https
+  }[this.urlObj.protocol.split(':')[0]]
   this.readable = opts.readable !== false
   this.writable = false
   this.length = opts.length || 0
@@ -23,7 +30,29 @@ function RandomAccessHTTP (url, opts) {
 inherits(RandomAccessMemory, events.EventEmitter)
 
 RandomAccessHTTP.prototype.open = function (cb) {
-  cb()
+  var self = this
+  var reqOpts = xtend(this.urlObj, {
+    method: 'HEAD',
+    headers: {
+      Connection: 'keep-alive'
+    }
+  })
+  this.reqOpts = reqOpts
+  this.client.request(reqOpts, function (res) {
+    if (res.statusCode !== 200) return cb(new Error('Bad response: ' + res.statusCode))
+    if (headersInvalid(res.headers)) {
+      return cb(new Error('Source doesn\' support \'accept-ranges\''))
+    }
+    self.opened = true
+    if (res.headers['content-length']) self.length = res.headers['content-length']
+    self.emit('open')
+    cb()
+  })
+}
+
+function headersInvalid (headers) {
+  if (!headers['accept-ranges']) return true
+  if (headers['accept-ranges'] !== 'bytes') return true
 }
 
 RandomAccessHTTP.prototype.write = function (offset, data, cb) {
